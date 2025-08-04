d5482ec453b4baf7795152a15656c06a9f5718e0
fayc <yingfc5000@outlook.com>
2025-08-04 21:13:08 +0800

    chore:  update deployment config - Add GitHub workflow for automated deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/assessDRSmodels-radar/' // Replace with your repository name
 })
