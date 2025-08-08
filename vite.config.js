2b55b0b82cecabca595d386b2acaad666d42cea9
fayc <yingfc5000@outlook.com>
2025-08-08 14:51:06 +0800

    chore(build-config): update Vite base path to NaRMBench_radar

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/assessDRSmodels-radar/' // Replace with your repository name
+  base: '/NaRMBench_radar/' // Replace with your repository name
 })
